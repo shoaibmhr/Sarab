9293483853d7f45e957fa49aba4d74ffe644c3d9
shoaibmhr <shoaibmhr895@gmail.com>
2026-08-01 02:26:15 +0500

    changing

--- src/website/layouts/MainLayout.jsx
+++ src/website/layouts/MainLayout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
-
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-
 const MainLayout = () => {
   return (
     <>
